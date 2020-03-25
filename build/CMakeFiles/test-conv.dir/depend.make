4b8b498eacccfb30e621a66a977b7d57d80f8d8a
pfzhang <pfzhang@cse.cuhk.edu.hk>
2020-03-25 09:36:29 +0800

    change to use Clion

--- build/CMakeFiles/test-conv.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test-conv.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pfzhang/workspace/quant_conv
-# Build directory: /home/pfzhang/workspace/quant_conv/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test-conv "test-conv")
-subdirs("googletest")
-subdirs("googlebenchmark")
